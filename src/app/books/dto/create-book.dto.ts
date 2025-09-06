e76452a2e41d74fcbe02ae227f037c0dda178bc9
Wellington Souza da Cunha <wellingtinsouza109000@gmail.com>
2025-09-06 15:12:30 -0300

    atualizando book

--- src/app/books/dto/create-book.dto.ts
+++ src/app/books/dto/create-book.dto.ts
@@ -1,1 +1,25 @@
-export class CreateBookDto {}
+import {
+    IsString,
+    IsNotEmpty,
+    IsOptional,
+    IsBoolean,
+} from 'class-validator';
+
+export class CreateBookDto {
+    
+    @IsString()
+    @IsNotEmpty()
+    title: string;
+
+    @IsString()
+    @IsNotEmpty()   
+    author: string;
+
+    @IsString()
+    @IsNotEmpty()   
+    category: string;
+
+    @IsBoolean()
+    @IsOptional()
+    available?: boolean;
+}
